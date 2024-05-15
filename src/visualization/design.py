2ea16d3bb410089cf66c9ce32f4ae7f8c6c9776a
dmitr <dmitrievvlad2701@gmail.com>
2024-05-15 09:42:15 +0300

    Обновленный макет frontend без функционала 15_05_24

--- src/visualization/design.py
+++ src/visualization/design.py
@@ -1,166 +1,226 @@
+# -*- coding: utf-8 -*-
+
 import os
+import random
 import sys
 
-import matplotlib
-
-matplotlib.use('Qt5Agg')
-from matplotlib.backends.backend_qt5agg import FigureCanvasAgg as FigureCanvas, \
+import pandas as pd
+from PyQt5 import QtCore, QtGui, QtWidgets
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas, \
     NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
 
-from src.calculation.auxiliary_functions import get_path
+from src.preparing.preparing_data import get_path
 
 
-class MyCanvas(FigureCanvas):
-
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
-        fig = Figure(figsize=(width, height), dpi=dpi)
-        self.axes = fig.add_subplot(111)
-        self.axes.plot([1, 4, 5], [2, 0, 7])
-        super(MyCanvas, self).__init__(fig)
-
-
-class MainWindow(QMainWindow):
+class App(QtWidgets.QMainWindow):
 
     def __init__(self):
         super().__init__()
-
-        # Настройки окна
-        self.setWindowTitle('Опорная сеть')
-        self.setGeometry(500, 200, 1000, 700)
-        self.createActions()
-        self.create_menu_bar()
-        self.btns()
-        self.createActions()
-        self.sc = MyCanvas(self, width=5, height=4, dpi=100)
-        self.toolbar = NavigationToolbar(self.sc)
-        self.layout = QVBoxLayout()
-        self.layout.addWidget(self.toolbar)
-        self.layout.addWidget(self.sc)
-        # widget = QWidget()
-        # widget.setLayout(self.layout)
+        self.left = 400
+        self.top = 300
+        self.width = 1200
+        self.height = 800
+        self.main_window()
+        self.setMenuWidget(CreateMenu())
         self.show()
 
+    def main_window(self):
+        self.setWindowTitle("Module GDIS")
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.setStyleSheet("background-color: lightgray;")
+        self.setMenuBar(CreateMenu())
+        widget = QtWidgets.QWidget(self)
+        self.setCentralWidget(widget)
+        hlayout = QtWidgets.QHBoxLayout(widget)
+        labels = WidgetLabels(self)
+        m = TablePlots()
+        hlayout.addWidget(labels, stretch=1)
+        hlayout.addWidget(m, stretch=3)
+
+
+class TablePlots(QtWidgets.QTabWidget):
+
+    def __init__(self):
+        QtWidgets.QTabWidget.__init__(self)
+        file = pd.ExcelFile(get_path() + '//output//' + 'out_file_geometry.xlsx')
+        for name in file.sheet_names[1:-1]:
+            self.addTab(WidgetPlot(), name)
+
+
+class PlotCanvas(FigureCanvas):
+
+    def __init__(self, parent=None, width=5, heigth=4, dpi=100):
+        fig = Figure(figsize=(width, heigth), dpi=dpi)
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+        FigureCanvas.setSizePolicy(self, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+        self.picture_plot()
+
+    def picture_plot(self):
+        data = [random.random() for i in range(250)]
+        ax = self.figure.add_subplot(111)
+        ax.plot(data, 'r-', linewidth=0.5)
+        ax.set_title('PyQt Matplotlib Example')
+        self.draw()
+
+
+class WidgetPlot(QtWidgets.QWidget):
+
+    def __init__(self, *args, **kwargs):
+        QtWidgets.QWidget.__init__(self, *args, **kwargs)
+        self.setLayout(QtWidgets.QVBoxLayout())
+        self.canvas = PlotCanvas(self, width=5, heigth=4, dpi=100)
+        self.toolbar = NavigationToolbar(self.canvas)
+        button_plot = QtWidgets.QComboBox(self)
+        button_plot.addItem('k = 1')
+        button_plot.addItem('k = 1.5')
+        button_plot.addItem('k = 2')
+        button_plot.addItem('k = 2.5')
+        button_plot.setFixedSize(200, 25)
+
+        self.layout().addWidget(button_plot)
+        self.layout().addWidget(self.toolbar)
+        self.layout().addWidget(self.canvas)
+
+
+class WidgetLabels(QtWidgets.QWidget):
+
+    def __init__(self, *args, **kwargs):
+        QtWidgets.QWidget.__init__(self, *args, **kwargs)
+        self.param_labels()
+
+    def param_labels(self):
+        # главный виджет со всеми окнами задания параметров расчета
+        main_box = QtWidgets.QVBoxLayout(self)
+
+        # горизонтальный виджет для задания имени файла/пути
+        box_data = QtWidgets.QHBoxLayout(self)
+        filepath = QtWidgets.QLineEdit()
+        file_button = QtWidgets.QPushButton("...")
+        file_button.clicked.connect(
+            lambda: filepath.setText(QtWidgets.QFileDialog.getOpenFileName(self, filter='*.csv *.xlsx')[0]))
+        file_button.setFixedSize(30, 25)
+        box_data.addWidget(QtWidgets.QLabel('Данные:'))
+        box_data.addWidget(filepath)
+        box_data.addWidget(file_button)
+
+        # горизонтальный виджет для ввода даты ГДИС
+        box_gdis_date = QtWidgets.QHBoxLayout(self)
+        box_gdis_date.addWidget(QtWidgets.QLabel('Дата ГДИС:'))
+        box_gdis_date.addWidget(QtWidgets.QLineEdit())
+
+        # горизонтальный виджет для рапределения ГДИС по годам
+        box_gdis_opt = QtWidgets.QHBoxLayout(self)
+        box_gdis_opt.addWidget(QtWidgets.QLabel('ГДИС по годам:'))
+        box_gdis_opt.addWidget(QtWidgets.QLineEdit())
+
+        # горизонтальный виджет для выбора критерия охвата скважин исследованиями
+        box_criteria = QtWidgets.QHBoxLayout(self)
+        edit_criteria = QtWidgets.QComboBox()
+        edit_criteria.addItem('True')
+        edit_criteria.addItem('False')
+        edit_criteria.setFixedSize(155, 25)
+        box_criteria.addWidget(QtWidgets.QLabel('Критерий охвата:'))
+        box_criteria.addWidget(edit_criteria)
+
+        # горизонтальный виджет для ввода максимального радиуса исследования
+        box_max_r = QtWidgets.QHBoxLayout(self)
+        box_max_r.addWidget(QtWidgets.QLabel('Максимальный R:'))
+        box_max_r.addWidget(QtWidgets.QLineEdit())
+
+        # горизонтальный виджет для ввода начального коэффициента увеличения радиуса
+        box_min_coef = QtWidgets.QHBoxLayout(self)
+        box_min_coef.addWidget(QtWidgets.QLabel('Минимальный коэф-т:'))
+        box_min_coef.addWidget(QtWidgets.QLineEdit())
+
+        # горизонтальный виджет для ввода начального коэффициента увеличения радиуса
+        box_max_coef = QtWidgets.QHBoxLayout(self)
+        box_max_coef.addWidget(QtWidgets.QLabel('Максимальный коэф-т:'))
+        box_max_coef.addWidget(QtWidgets.QLineEdit())
+
+        # горизонтальный виджет для ввода шага коэффициента кратного увеличения радиуса
+        box_step_coef = QtWidgets.QHBoxLayout(self)
+        box_step_coef.addWidget(QtWidgets.QLabel('Шаг коэф-та:'))
+        box_step_coef.addWidget(QtWidgets.QLineEdit())
+
+        # добавление всех виджетов в главный виджет
+        main_box.addLayout(box_data)
+        main_box.addLayout(box_gdis_date)
+        main_box.addLayout(box_gdis_opt)
+        main_box.addLayout(box_criteria)
+        main_box.addLayout(box_max_r)
+        main_box.addLayout(box_min_coef)
+        main_box.addLayout(box_max_coef)
+        main_box.addLayout(box_step_coef)
+
+        main_box.setAlignment(QtCore.Qt.AlignTop)
+
+
+class CreateMenu(QtWidgets.QMenuBar):
+
+    def __init__(self):
+        QtWidgets.QMenuBar.__init__(self)
+        self.create_menu_bar()
+        self.createActions()
+
     def create_menu_bar(self):
-        menuBar = self.menuBar()
-        # Creating menu using a QMenu object
-        fileMenu = QMenu("&File", self)
-        menuBar.addMenu(fileMenu)
-        fileMenu.addAction(self.newAction)
-        fileMenu.addAction(self.openAction)
-        fileMenu.addAction(self.saveAction)
-        fileMenu.addAction(self.exitAction)
-        # Creating menu using a title
-        editMenu = menuBar.addMenu("&Edit")
-        editMenu.addAction(self.copyAction)
-        editMenu.addAction(self.pasteAction)
-        editMenu.addAction(self.cutAction)
+        fileMenu = QtWidgets.QMenu("&File", self)
+        newAction = QtWidgets.QAction("&New", self)
+        newAction.setShortcut('Ctrl+N')
+        newAction.setIcon(QtGui.QIcon(":file-new.svg"))
+        openAction = QtWidgets.QAction(QtGui.QIcon(":file-open.svg"), "&Open...", self)
+        saveAction = QtWidgets.QAction(QtGui.QIcon(":file-save.svg"), "&Save", self)
+        exitAction = QtWidgets.QAction("&Exit", self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.triggered.connect(lambda: QtWidgets.QApplication.quit())
+        fileMenu.addAction(newAction)
+        fileMenu.addAction(openAction)
+        fileMenu.addAction(saveAction)
+        fileMenu.addAction(exitAction)
+        self.addMenu(fileMenu)
 
-        helpMenu = menuBar.addMenu("&Help")
-        helpMenu.addAction(self.referenceAction)
+        editMenu = QtWidgets.QMenu("&Edit", self)
+        copyAction = QtWidgets.QAction(QtGui.QIcon(":edit-copy.svg"), "&Copy", self)
+        pasteAction = QtWidgets.QAction(QtGui.QIcon(":edit-paste.svg"), "&Paste", self)
+        cutAction = QtWidgets.QAction(QtGui.QIcon(":edit-cut.svg"), "&Cut", self)
+        editMenu.addAction(copyAction)
+        editMenu.addAction(pasteAction)
+        editMenu.addAction(cutAction)
+        self.addMenu(editMenu)
 
-    def btns(self):
-        # текст файла с данными
-        text_file = QLabel('Данные:', self)
-        # text_file.setStyleSheet('font-size: 14px')
-        text_file.move(10, 32)
-        text_file.setFixedSize(110, 20)
-        # Окно вывода пути к файлу с данными
-        layout = QLineEdit(self)
-        layout.setFixedSize(150, 20)
-        layout.setStyleSheet("background-color: white; border: 1px solid black")
-        layout.move(120, 30)
-        # Кнопка загрузки пути к файлу со скважинами
-        download_data = QPushButton('...', self)
-        download_data.setFixedSize(20, 21)
-        download_data.move(270, 30)
-        download_data.clicked.connect(
-            lambda: layout.setText(QFileDialog.getOpenFileName(self, filter='*.csv *.xlsx')[0]))
-        self.data_path = download_data.text()
-
-        # текст даты последнего ГДИС
-        text_gdis_date = QLabel('Дата ГДИС:', self)
-        text_gdis_date.move(10, 104)
-        # Окно для ввода даты последнего ГДИС
-        layout_gdis_date = QLineEdit(self)
-        layout_gdis_date.setFixedSize(150, 20)
-        layout_gdis_date.setStyleSheet("background-color: white; border: 1px solid black")
-        layout_gdis_date.move(120, 110)
-
-        # текст gdis_option
-        text_gdis_opt = QLabel('ГДИС по годам:', self)
-        # text_gdis_opt.setStyleSheet('font-size: 14px')
-        text_gdis_opt.setFixedSize(250, 16)
-        text_gdis_opt.move(10, 130)
-        text_gdis_opt.setFixedSize(210, 20)
-        # Окно ввода кол-ва лет доп. исследований ГДИС
-        layout_gdis_opt = QComboBox(self)
-        layout_gdis_opt.addItem('нет')
-        layout_gdis_opt.addItem('1')
-        layout_gdis_opt.addItem('2')
-        layout_gdis_opt.setFixedSize(150, 20)
-        layout_gdis_opt.move(120, 130)
-
-        # текст опции охвата траектории скважины (процент охвата траектории или охват при попадании любой точки)
-        text_include = QLabel('Критерий охвата:', self)
-        text_include.move(10, 146)
-        text_gdis_opt.setFixedSize(210, 20)
-        # Окно ввода критерия охвата скважин
-        layout_gdis_opt = QComboBox(self)
-        layout_gdis_opt.addItem('True')
-        layout_gdis_opt.addItem('False')
-        layout_gdis_opt.setFixedSize(150, 20)
-        layout_gdis_opt.move(120, 151)
-
-        # текст ограничения оптимального радиуса исследования
-        text_limit_radius = QLabel('Максимальный R:', self)
-        text_limit_radius.move(10, 171)
-        text_limit_radius.setFixedSize(110, 20)
-        # Окно для ввода ограничения оптимального радиуса исследования
-        layout_limit_radius = QLineEdit(self)
-        layout_limit_radius.setFixedSize(150, 20)
-        layout_limit_radius.setStyleSheet("background-color: white; border: 1px solid black")
-        layout_limit_radius.move(120, 171)
-
-        # текст mult_coef
-        text_mult = QLabel('Коэф-ты для R:', self)
-        # text_mult.setStyleSheet('font-size: 14px')
-        text_mult.move(10, 211)
-        text_mult.setFixedSize(110, 20)
-        # Окно ввода списка коэффициентов для увеличения радиуса исследования
-        layout_pvt = QLineEdit(self)
-        layout_pvt.setFixedSize(150, 20)
-        layout_pvt.setStyleSheet("background-color: white; border: 1px solid black")
-        layout_pvt.move(120, 211)
-        # layout_pvt.setText(' ,'.join(str(layout_pvt.text()).split(',')))
+        helpMenu = QtWidgets.QMenu("&Help", self)
+        referenceAction = QtWidgets.QAction("&Reference", self)
+        referenceAction.triggered.connect(lambda: os.startfile(str(get_path() + '/README.txt')))
+        helpMenu.addAction(referenceAction)
+        self.addMenu(helpMenu)
 
     def createActions(self):
         # download action
 
-        self.download = QAction("&Open...", self)
+        download = QtWidgets.QAction("&Open...", self)
         # self.download.triggered.connect(lambda: QApplication.quit())
         # File actions
-        self.newAction = QAction("&New", self)
-        self.newAction.setShortcut('Ctrl+N')
-        self.newAction.setIcon(QIcon(":file-new.svg"))
+        newAction = QtWidgets.QAction("&New", self)
+        newAction.setShortcut('Ctrl+N')
+        newAction.setIcon(QtGui.QIcon(":file-new.svg"))
         # self.newAction.triggered.connect(lambda: QFileDialog.getOpenFileName())
-        self.openAction = QAction(QIcon(":file-open.svg"), "&Open...", self)
-        self.saveAction = QAction(QIcon(":file-save.svg"), "&Save", self)
-        self.exitAction = QAction("&Exit", self)
-        self.exitAction.setShortcut('Ctrl+Q')
-        self.exitAction.triggered.connect(lambda: QApplication.quit())
+        openAction = QtWidgets.QAction(QtGui.QIcon(":file-open.svg"), "&Open...", self)
+        saveAction = QtWidgets.QAction(QtGui.QIcon(":file-save.svg"), "&Save", self)
+        exitAction = QtWidgets.QAction("&Exit", self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.triggered.connect(lambda: QtWidgets.QApplication.quit())
         # Edit actions
-        self.copyAction = QAction(QIcon(":edit-copy.svg"), "&Copy", self)
-        self.pasteAction = QAction(QIcon(":edit-paste.svg"), "&Paste", self)
-        self.cutAction = QAction(QIcon(":edit-cut.svg"), "&Cut", self)
+        copyAction = QtWidgets.QAction(QtGui.QIcon(":edit-copy.svg"), "&Copy", self)
+        pasteAction = QtWidgets.QAction(QtGui.QIcon(":edit-paste.svg"), "&Paste", self)
+        cutAction = QtWidgets.QAction(QtGui.QIcon(":edit-cut.svg"), "&Cut", self)
         # Help actions
-        self.referenceAction = QAction("&Reference", self)
-        self.referenceAction.triggered.connect(lambda: os.startfile(str(get_path() + '/README.txt')))
+        referenceAction = QtWidgets.QAction("&Reference", self)
+        referenceAction.triggered.connect(lambda: os.startfile(str(get_path() + '/README.txt')))
 
 
-app = QApplication(sys.argv)
-window = MainWindow()
-window.show()
-app.exec()
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
