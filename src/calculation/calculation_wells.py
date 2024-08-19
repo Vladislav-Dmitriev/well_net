7fa076c7702dff3b7e5eb5d309e270ab2c2fb725
Vladislav-Dmitriev <dmitrievvlad2701@gmail.com>
2024-08-19 16:22:27 +0300

    calculation_wells.py закомментирована строка обнуления времени исследования пьезометров 19-08-2024

--- src/calculation/calculation_wells.py
+++ src/calculation/calculation_wells.py
@@ -349,7 +349,7 @@ def calc_contour(df_prod_wells, df_piez_wells, df_inj_wells, df_proj_wells, df_r
         logger.info('Incorrect value of separation_by_years parameter or limit_radius_coeff')
 
     # обнуление времени исследования пьезометрических скважин
-    df_result.loc[df_result['fond'] == 'ПЬЕЗ', 'research_time'] = 0
+    # df_result.loc[df_result['fond'] == 'ПЬЕЗ', 'research_time'] = 0
 
     # поиск охвата проектного фонда скважинами из ОС
     if not df_proj_wells.empty:
